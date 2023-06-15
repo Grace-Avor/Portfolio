a918c9e7e308ebbfd71446d70ef432985db55b7d
Grace-Avor <graceavor2021@gmail.com>
2023-06-15 23:21:09 +0100

    update

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -2,3 +2,12 @@ function darkMode() {
     var element = document.body;
     element.classList.toggle("dark-mode");
   }
+
+  var myForm = document.getElementById("myForm");
+      var result = document.getElementById("result");
+      function submitForm(event) {
+         event.preventDefault();
+         myForm.submit();
+
+         result.innerHTML = "<b>Thank you for reaching out</b>"
+      }
