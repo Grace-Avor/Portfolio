f49360e0423b7903eb962461b4ab88a45f7faaf3
Grace-Avor <graceavor2021@gmail.com>
2023-09-26 14:04:20 +0100

    Added opacity

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -6,17 +6,16 @@ myForm.addEventListener("submit", (e)=> {
 
 })*/
 
-
 function darkMode() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
-  }
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
 
-  function myFunction() {
-    var links = document.getElementById("myLinks");
-    if (links.style.display === "block") {
-      links.style.display = "none";
-    } else {
-      links.style.display = "block";
-    }
+function myFunction() {
+  var links = document.getElementById("myLinks");
+  if (links.style.display === "block") {
+    links.style.display = "none";
+  } else {
+    links.style.display = "block";
   }
+}
