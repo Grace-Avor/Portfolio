26bd9bb07d41c8316bb5fcab58450344da81c773
Grace-Avor <graceavor2021@gmail.com>
2023-09-26 12:50:55 +0100

    Added a hamburger

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -11,3 +11,12 @@ function darkMode() {
     var element = document.body;
     element.classList.toggle("dark-mode");
   }
+
+  function myFunction() {
+    var links = document.getElementById("myLinks");
+    if (links.style.display === "block") {
+      links.style.display = "none";
+    } else {
+      links.style.display = "block";
+    }
+  }
